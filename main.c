525c2c23c79faf5a541fc12dce54069705ede733
Mika Tuupola <tuupola@appelsiini.net>
2022-09-17 02:06:46 +0700

    Add changes required by HAGL 0.6.0 (#1)

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ SPDX-License-Identifier: MIT-0
 #include <sys/time.h>
 
 #include <hagl_hal.h>
-#include <bitmap.h>
+#include <hagl/bitmap.h>
 #include <hagl.h>
 #include <font6x9.h>
 #include <fps.h>
@@ -41,13 +41,13 @@ SPDX-License-Identifier: MIT-0
 
 static uint8_t current_demo = 1;
 volatile bool fps_flag = false;
-volatile bool switch_flag = true;
+volatile bool switch_flag = false;
 volatile bool flush_flag = true;
-static float fx_fps;
-static float fb_fps;
+static fps_instance_t fps;
+static aps_instance_t aps;
 static uint32_t drawn = 0;
+static hagl_backend_t *display;
 
-static bitmap_t *bb;
 wchar_t message[32];
 
 static char primitive[19][32] = {
@@ -86,7 +86,7 @@ void polygon_demo()
     int16_t y4 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
     int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
-    hagl_draw_polygon(5, vertices, colour);
+    hagl_draw_polygon(display, 5, vertices, colour);
 }
 
 void fill_polygon_demo()
@@ -103,7 +103,7 @@ void fill_polygon_demo()
     int16_t y4 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
     int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
-    hagl_fill_polygon(5, vertices, colour);
+    hagl_fill_polygon(display, 5, vertices, colour);
 }
 
 void circle_demo()
@@ -112,7 +112,7 @@ void circle_demo()
     int16_t y0 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     uint16_t r = (rand() % 40);
     color_t colour = rand() % 0xffff;
-    hagl_draw_circle(x0, y0, r, colour);
+    hagl_draw_circle(display, x0, y0, r, colour);
 }
 
 void fill_circle_demo()
@@ -121,7 +121,7 @@ void fill_circle_demo()
     int16_t y0 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     uint16_t r = (rand() % 40);
     color_t colour = rand() % 0xffff;
-    hagl_fill_circle(x0, y0, r, colour);
+    hagl_fill_circle(display, x0, y0, r, colour);
 }
 
 void ellipse_demo()
@@ -131,7 +131,7 @@ void ellipse_demo()
     uint16_t a = (rand() % 40) + 20;
     uint16_t b = (rand() % 40) + 20;
     color_t colour = rand() % 0xffff;
-    hagl_draw_ellipse(x0, y0, a, b, colour);
+    hagl_draw_ellipse(display, x0, y0, a, b, colour);
 }
 
 void fill_ellipse_demo()
@@ -141,7 +141,7 @@ void fill_ellipse_demo()
     uint16_t a = (rand() % 40) + 20;
     uint16_t b = (rand() % 40) + 20;
     color_t colour = rand() % 0xffff;
-    hagl_fill_ellipse(x0, y0, a, b, colour);
+    hagl_fill_ellipse(display, x0, y0, a, b, colour);
 }
 
 void line_demo()
@@ -151,7 +151,7 @@ void line_demo()
     int16_t x1 = (rand() % DISPLAY_WIDTH + 20) - 20;
     int16_t y1 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_draw_line(x0, y0, x1, y1, colour);
+    hagl_draw_line(display, x0, y0, x1, y1, colour);
 }
 
 void vline_demo()
@@ -160,7 +160,7 @@ void vline_demo()
     int16_t y0 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     int16_t w = (rand() % DISPLAY_WIDTH + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_draw_vline(x0, y0, w, colour);
+    hagl_draw_vline(display, x0, y0, w, colour);
 }
 
 
@@ -170,7 +170,7 @@ void hline_demo()
     int16_t y0 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     int16_t h = (rand() % DISPLAY_WIDTH + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_draw_vline(x0, y0, h, colour);
+    hagl_draw_vline(display, x0, y0, h, colour);
 }
 
 void rectangle_demo()
@@ -180,7 +180,7 @@ void rectangle_demo()
     int16_t x1 = (rand() % DISPLAY_WIDTH + 20) - 20;
     int16_t y1 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_draw_rectangle(x0, y0, x1, y1, colour);
+    hagl_draw_rectangle(display, x0, y0, x1, y1, colour);
 }
 
 void fill_rectangle_demo()
@@ -190,7 +190,7 @@ void fill_rectangle_demo()
     int16_t x1 = (rand() % DISPLAY_WIDTH + 20) - 20;
     int16_t y1 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_fill_rectangle(x0, y0, x1, y1, colour);
+    hagl_fill_rectangle(display, x0, y0, x1, y1, colour);
 }
 
 void put_character_demo()
@@ -200,7 +200,7 @@ void put_character_demo()
 
     color_t colour = rand() % 0xffff;
     char ascii = rand() % 127;
-    hagl_put_char(ascii, x0, y0, colour, font6x9);
+    hagl_put_char(display, ascii, x0, y0, colour, font6x9);
 }
 
 void put_text_demo()
@@ -210,7 +210,7 @@ void put_text_demo()
 
     color_t colour = rand() % 0xffff;
 
-    hagl_put_text(L"YO¡ MTV raps2♥", x0, y0, colour, font6x9);
+    hagl_put_text(display, L"YO¡ MTV raps2♥", x0, y0, colour, font6x9);
 }
 
 void put_pixel_demo()
@@ -218,7 +218,7 @@ void put_pixel_demo()
     int16_t x0 = (rand() % DISPLAY_WIDTH + 20) - 20;
     int16_t y0 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_put_pixel(x0, y0, colour);
+    hagl_put_pixel(display, x0, y0, colour);
 }
 
 void triangle_demo()
@@ -230,7 +230,7 @@ void triangle_demo()
     int16_t x2 = (rand() % DISPLAY_WIDTH + 20) - 20;
     int16_t y2 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_draw_triangle(x0, y0, x1, y1, x2, y2, colour);
+    hagl_draw_triangle(display, x0, y0, x1, y1, x2, y2, colour);
 }
 
 void fill_triangle_demo()
@@ -242,22 +242,22 @@ void fill_triangle_demo()
     int16_t x2 = (rand() % DISPLAY_WIDTH + 20) - 20;
     int16_t y2 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     color_t colour = rand() % 0xffff;
-    hagl_fill_triangle(x0, y0, x1, y1, x2, y2, colour);
+    hagl_fill_triangle(display, x0, y0, x1, y1, x2, y2, colour);
 }
 
 void rgb_demo()
 {
-    uint16_t red = hagl_color(255, 0, 0);
-    uint16_t green = hagl_color(0, 255, 0);
-    uint16_t blue = hagl_color(0, 0, 255);
+    uint16_t red = hagl_color(display, 255, 0, 0);
+    uint16_t green = hagl_color(display, 0, 255, 0);
+    uint16_t blue = hagl_color(display, 0, 0, 255);
 
     int16_t x0 = 0;
     int16_t x1 = DISPLAY_WIDTH / 3;
     int16_t x2 = 2 * x1;
 
-    hagl_fill_rectangle(x0, 0, x1 - 1, DISPLAY_HEIGHT, red);
-    hagl_fill_rectangle(x1, 0, x2 - 1, DISPLAY_HEIGHT, green);
-    hagl_fill_rectangle(x2, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, blue);
+    hagl_fill_rectangle(display, x0, 0, x1 - 1, DISPLAY_HEIGHT, red);
+    hagl_fill_rectangle(display, x1, 0, x2 - 1, DISPLAY_HEIGHT, green);
+    hagl_fill_rectangle(display, x2, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, blue);
 }
 
 void round_rectangle_demo()
@@ -268,7 +268,7 @@ void round_rectangle_demo()
     int16_t y1 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     int16_t r = rand() % 10;
     color_t colour = rand() % 0xffff;
-    hagl_draw_rounded_rectangle(x0, y0, x1, y1, r, colour);
+    hagl_draw_rounded_rectangle(display, x0, y0, x1, y1, r, colour);
 }
 
 void fill_round_rectangle_demo()
@@ -279,7 +279,7 @@ void fill_round_rectangle_demo()
     int16_t y1 = (rand() % DISPLAY_HEIGHT + 20) - 20;
     int16_t r = rand() % 10;
     color_t colour = rand() % 0xffff;
-    hagl_fill_rounded_rectangle(x0, y0, x1, y1, r, colour);
+    hagl_fill_rounded_rectangle(display, x0, y0, x1, y1, r, colour);
 }
 
 bool switch_timer_callback(struct repeating_timer *t) {
@@ -300,27 +300,23 @@ bool flush_timer_callback(struct repeating_timer *t) {
 
 int main()
 {
+    stdio_init_all();
+    sleep_ms(5000);
+
     struct repeating_timer switch_timer;
     struct repeating_timer fps_timer;
     struct repeating_timer flush_timer;
 
-    color_t red = hagl_color(255, 0, 0);
-    color_t green = hagl_color(0, 255, 0);
-    color_t blue = hagl_color(0, 0, 255);
+    display = hagl_init();
 
-    stdio_init_all();
+    color_t red = hagl_color(display, 255, 0, 0);
+    color_t green = hagl_color(display, 0, 255, 0);
+    color_t blue = hagl_color(display, 0, 0, 255);
 
-    sleep_ms(5000);
+    printf("Hello world!\n");
 
-    bb = hagl_init();
-    if (bb) {
-        printf("Back buffer: %dx%dx%d\r\n", bb->width, bb->height, bb->depth);
-    } else {
-        printf("No back buffer\r\n");
-    }
-
-    hagl_clear_screen();
-    hagl_set_clip_window(0, 20, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 21);
+    hagl_clear(display);
+    hagl_set_clip_window(display, 0, 20, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 21);
 
     add_repeating_timer_ms(10000, switch_timer_callback, NULL, &switch_timer);
     add_repeating_timer_ms(1000, fps_timer_callback, NULL, &fps_timer);
@@ -348,46 +344,48 @@ int main()
     demo[17] = put_character_demo;
     demo[18] = put_text_demo;
 
-    while (1) {
+    fps_init(&fps);
+    aps_init(&aps);
 
+    while (1) {
         (*demo[current_demo])();
         drawn++;
 
-#ifdef HAGL_HAL_USE_DOUBLE_BUFFER
+#ifdef HAGL_HAS_HAL_BACK_BUFFER
         if (flush_flag) {
             flush_flag = 0;
-            hagl_flush();
-            fb_fps = fps();
+            hagl_flush(display);
+            fps_update(&fps);
         }
-#endif /* HAGL_HAL_USE_DOUBLE_BUFFER */
+#endif /* HAGL_HAS_HAL_BACK_BUFFER */
 
         if (switch_flag) {
             switch_flag = 0;
-            printf("%d %s per second, FB %d FPS\r\n", (uint32_t)fx_fps, primitive[current_demo], (uint32_t)fb_fps);
+            printf("%d %s per second, display %d FPS\r\n", (uint32_t)aps.current, primitive[current_demo], (uint32_t)fps.current);
             current_demo = (current_demo + 1) % 18;
-            fx_fps = aps(APS_RESET);
+            aps_reset(&aps);
             drawn = 0;
         }
 
         if (fps_flag) {
             fps_flag = 0;
 
-            fx_fps = aps(drawn);
+            aps_update(&aps, drawn);
             drawn = 0;
 
-            hagl_set_clip_window(0, 0, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 1);
+            hagl_set_clip_window(display, 0, 0, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 1);
 
             /* Print the message on upper left corner. */
-            swprintf(message,  sizeof(message), L"%.*f APS       ", 0, fx_fps);
-            hagl_put_text(message, 8, 4, green, font6x9);
+            swprintf(message,  sizeof(message), L"%.*f APS       ", 0, aps.current);
+            hagl_put_text(display, message, 8, 4, green, font6x9);
 
-#ifdef HAGL_HAL_USE_DOUBLE_BUFFER
+#ifdef HAGL_HAS_HAL_BACK_BUFFER
             /* Print the message on lower right corner. */
-            swprintf(message, sizeof(message), L"%.*f FPS  ", 0, fb_fps);
-            hagl_put_text(message, DISPLAY_WIDTH - 40, DISPLAY_HEIGHT - 14, green, font6x9);
-#endif /* HAGL_HAL_USE_DOUBLE_BUFFER */
+            swprintf(message, sizeof(message), L"%.*f FPS  ", 0, fps.current);
+            hagl_put_text(display, message, DISPLAY_WIDTH - 40, DISPLAY_HEIGHT - 14, green, font6x9);
+#endif /* HAGL_HAS_HAL_BACK_BUFFER */
 
-            hagl_set_clip_window(0, 20, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 21);
+            hagl_set_clip_window(display, 0, 20, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 21);
         }
     }
 
